0649f34bd9af98f8b948f2af1cccf22611a737ad
Bell Wang (王 宝真) <baozwang@tesla.com>
2021-11-11 21:30:57 +0800

    init

---
+++ src/main/java/com/sonarqube/Animal.java
@@ -0,0 +1,9 @@
+package com.sonarqube;
+
+public class Animal {
+
+    public static void main(String[] args) {
+        String a = "dd";
+        System.out.println("this is Animal " + a);
+    }
+}
